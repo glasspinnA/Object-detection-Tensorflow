bef9f5e05eda2ca82c93675c25820a055f74e417
glasspinnA <ozze-1995@hotmail.com>
2019-01-23 14:28:44 +0100

    possible to predict dynamic images

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import jonna2 from './jonna2.jpg';
 import './App.css';
 
 import * as cocoSsd from '@tensorflow-models/coco-ssd';
-import * as tf from '@tensorflow/tfjs';
+import '@tensorflow/tfjs';
 
 
 class App extends Component {
@@ -14,44 +13,81 @@ class App extends Component {
 
     this.state = {
       model: null,
+      track: null,
     }
 
   }
 
-  componentDidMount() {
-    this.loadModel();
+  componentDidMount = async () => {
+    const model = await this.loadModel();
+    this.initWebcam(model);
   }
 
   /**
-   * Function to load the image to be predicted
+   * Function to init the webcam and stream to the video tag
    */
-  loadPicture() {
-    return document.getElementById("img");
+  initWebcam = async (model) => {
+    const video = document.getElementById("webcam");
+    this.streamVideo(video, model);
   }
+
+  /**
+   * Function to stream the video to the model
+   * @param {*} video - The video to be streamed
+   * @param {*} model - The loaded deep learning model, which is used to predict the objects 
+   */
+  streamVideo(video, model) {
+    const constrains = { video: { facingMode: 'user' } };
+
+    if (navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia(constrains)
+        .then(mediaStream => {
+
+          video.srcObject = mediaStream;
+
+          video.onloadedmetadata = () => {
+            this.predictObject(video, model);
+          }
+
+          const track = mediaStream.getTracks()[0];
+          this.setState({ track: track });
+        })
+        .catch(error => {
+          console.log(error.message);
+        })
+    }
+  }
+
 
   /**
    * Function to load the model
    */
   loadModel = async () => {
-    const image = this.loadPicture();
-    await cocoSsd.load().then(model => {
-      this.predict(model, image);
-    })
+    return await cocoSsd.load();
   }
 
   /**
    * Function to predict what object the model sees in the image
    */
-  predict = async (model, image) => {
-    await model.detect(image).then(prediciton => {
-      console.log(prediciton);
+  predictObject = async (image, model) => {
+    model.detect(image).then(predictions => {
+      console.log(JSON.stringify(predictions));
+      requestAnimationFrame(() => {
+        this.predictObject(image, model)
+      })
     })
   }
+
+  stopWebcam = () => {
+    this.state.track.stop();
+  }
+
 
   render() {
     return (
       <div className="App">
-        <img id="img" src={jonna2} />
+        <video autoPlay muted width="400" height="400" id="webcam" />
+        <button id="btnStop" onClick={this.stopWebcam}>Stop camera</button>
       </div>
     );
   }
