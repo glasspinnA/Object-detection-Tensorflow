f8ca77ea0e3bbda8b2b5a0aa7407fad3245deb78
glasspinnA <ozze-1995@hotmail.com>
2019-01-24 12:47:14 +0100

    finishing touch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-import jonna2 from './jonna2.jpg';
 import './App.css';
-
 import * as cocoSsd from '@tensorflow-models/coco-ssd';
 import '@tensorflow/tfjs';
 
@@ -15,12 +13,14 @@ class App extends Component {
       model: null,
       track: null,
       req: null,
+      canvas: null,
     }
 
   }
 
   componentDidMount = async () => {
     const model = await this.loadModel();
+    this.initCanvas();
     this.initWebcam(model);
   }
 
@@ -30,6 +30,18 @@ class App extends Component {
   initWebcam = async (model) => {
     const video = document.getElementById("webcam");
     this.streamVideo(video, model);
+  }
+
+  /**
+   * Function to init the canvas 
+   */
+  initCanvas() {
+    const canvas = document.getElementById("canvas");
+    const context = canvas.getContext("2d");
+
+    this.setState({
+      context: context
+    });
   }
 
   /**
@@ -61,6 +73,58 @@ class App extends Component {
 
 
   /**
+   * Function thats draws a rectangle around the detected object
+   * @param {*} prediciton 
+   */
+  drawRectangle(prediction) {
+    const context = this.state.context;
+    context.clearRect(0, 0, 400, 400);
+    try {
+
+      for (let i = 0; i < prediction.length; i++) {
+        const coordinates = this.getCoordinates(prediction[i]);
+        const objectName = this.getObjectName(prediction[i]);
+
+        context.beginPath();
+        context.fillText(objectName, coordinates[0], coordinates[1] - 10);
+        context.rect(coordinates[0], coordinates[1], coordinates[2], coordinates[3]);
+        context.stroke();
+
+      }
+    } catch (error) {
+      console.log(error);
+
+    }
+  }
+
+  /**
+   * Function to get the name of the object that is predicted
+   * @param {*} prediciton 
+   */
+  getObjectName(prediciton) {
+    try {
+      return prediciton.class;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  /**
+   * Function to get coordinates for the object that is predicted 
+   * @param {*} prediciton 
+   */
+  getCoordinates(prediciton) {
+    try {
+      return prediciton.bbox;
+    }
+    catch (error) {
+      console.log(error);
+    }
+
+
+  }
+
+  /**
    * Function to load the model
    */
   loadModel = async () => {
@@ -72,7 +136,9 @@ class App extends Component {
    */
   predictObject = async (image, model) => {
     model.detect(image).then(predictions => {
-      console.log(JSON.stringify(predictions));
+      console.log((predictions))
+      this.drawRectangle(predictions);
+
       const myReq = requestAnimationFrame(() => {
         this.predictObject(image, model)
       })
@@ -99,8 +165,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <video autoPlay muted width="400" height="400" id="webcam" />
-        <canvas width="400" height="400" />
+        <div className="video-container">
+          <canvas id="canvas" width="400" height="400" />
+          <video autoPlay muted width="400" height="400" id="webcam" />
+        </div>
         <button id="btnStop" onClick={this.stopWebcam}>Stop Camera</button>
         <button id="btnStopModel" onClick={this.stopModel}>Stop Model</button>
       </div>
